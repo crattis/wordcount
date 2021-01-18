f504b9515a08b51db5b8c8eb9a9c72f58d50d622
Chris J <crattis@users.noreply.github.com>
2021-01-18 17:40:54 -0500

    cleaned up case issue, added comments.

--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -1,20 +1,23 @@
-# Count the occurrences of the words in local files
+# Count the occurrences of the words in local files.
 import os
 
 
 def main():
     word_count = {}
-    # TODO: For loop for local files
+    # Find HTML files in the current directory for Frequency Analysis.
     for html_file in os.listdir('.'):
         if html_file.endswith(".html"):
             with open(html_file, 'r') as file:
+                # Get count the words used in the HTML file to a shared
+                # dictionary.
                 content = ' '.join(line for line in file.read().splitlines())
                 content = content.split(' ')
                 for word in content:
-                    if word not in word_count:
-                        word_count[word] = 1
+                    if word.lower() not in word_count:
+                        word_count[word.lower()] = 1
                     else:
-                        word_count[word] += 1
+                        word_count[word.lower()] += 1
+    # sort dictionary by value.
     word_list = sorted(word_count.items(),
                        key=lambda item: item[1], reverse=True)
     for counted in word_list:
